1f2efc12c68e16ab24228bde6fd5771c1b2373ef
Daniel Cender <danielcender@gmail.com>
2020-01-22 23:31:54 -0600

    feat: Semaphores project finished, using sem_open not sem_init

--- Wk2/MonitorsAndSemaphores/Semaphores/Semaphores.c
+++ Wk2/MonitorsAndSemaphores/Semaphores/Semaphores.c
@@ -8,43 +8,55 @@
 */
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <pthread.h>
+#include <errno.h>
+#include <string.h>
 #include <semaphore.h>
 #include <unistd.h>
 
-sem_t mutex;
+#define SEM_MUTEX_NAME "/sem-mutex"
+
+sem_t *sem_mutex;
 char alphabet[] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
 int idx;
 
-/**
- * Func that prints out 2 chars of the alphabet array
- */
-void* printAlphabet(void* arg) { //function which act like thread
-	while(idx <= 25) {
-   sem_wait(&mutex); //wait state on semaphore
-	 if(idx > 25) break;
-	 printf("\nEntered..\n");
-	 printf("%c\n", alphabet[idx]);
-	 printf("Index at: %i", idx);
-	 ++idx; // increment index
-	 printf("\nJust Exiting...\n");
-   sem_post(&mutex); //send message to free up mutex
+void* thread_f(void* arg) {
+	while(1) {
+		if(idx < 25) {
+			if (sem_wait (sem_mutex) == -1) {
+	    	perror ("sem_wait: mutex_sem"); exit (1);
+      }
+			printf("\nThread %i, Entered...\n", pthread_self());
+			printf("%c\n", alphabet[idx]);
+			printf("Index at: %i", idx);
+			++idx; // increment index
+			printf("\nJust Exiting...\n");
+			// Release mutex sem: V (mutex_sem)
+      if (sem_post (sem_mutex) == -1) {
+	    perror ("sem_post: mutex_sem"); exit (1);
+      }
+		} else {
+			return NULL;
+		}
 	}
-	pthread_exit(0);
 }
 
-
 main() {
-	idx = 0; // max of 25, total length of alphabet
+	idx = 0; // max of 25, total length of alphabet (26 chars)
 
-	// init semaphore to 0, for only this process
-   sem_init(&mutex, 0, 1);
+	//  mutual exclusion semaphore, mutex_sem with an initial value 1.
+    if ((sem_mutex = sem_open (SEM_MUTEX_NAME, O_CREAT, 0660, 1)) == SEM_FAILED) {
+        perror ("sem_open"); exit (1);
+    }
    pthread_t th1,th2;
 	 //Create threads
-   pthread_create(&th1,NULL,printAlphabet,NULL);
-   pthread_create(&th2,NULL,printAlphabet,NULL);
+   pthread_create(&th1,NULL,thread_f,NULL);
+   pthread_create(&th2,NULL,thread_f,NULL);
    //Join threads with the main thread
    pthread_join(th1,NULL);
    pthread_join(th2,NULL);
-   sem_destroy(&mutex);
+  if (sem_unlink (SEM_MUTEX_NAME) == -1) {
+    perror ("sem_unlink"); exit (1);
+  }
 }
