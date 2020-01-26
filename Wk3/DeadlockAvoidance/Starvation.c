ed2b539d27f3b04da5dc1f1e4c230cb61ad52438
Daniel Cender <danielcender@gmail.com>
2020-01-25 23:40:20 -0600

    feat: finished base program, need to add fprintf now and document

--- Wk3/DeadlockAvoidance/Starvation.c
+++ Wk3/DeadlockAvoidance/Starvation.c
@@ -1,9 +1,21 @@
 /**
  *
  * Author: Daniel cender
- * Date: 01/23/2020
+ * Date: 01/25/2020
  *
- * Description:
+ * Description: This program illustrates how using a timeout to restart threads
+ *  to avoid deadlock is a dangerous way to potentially have your program forever
+ *  awaiting conditions that will never be met.
+ *
+ * In this program, we're setting up the scenario where the process at
+ * threads[0] runs for longer than the other threads we're spinning up.
+ *
+ * The short and long timeouts have been set to reflect a particular scenario.
+ * With SHORT_TIMEOUT == 4 and the LONG_TIMEOUT == 10, we get a situation where
+ * our shorter-running threads will run twice before the longer thread finishes it's first
+ * run. It'll then start another 10second run before our shorter threads can lock the mutex.
+ *
+ * So, in this case, our shorter threads never get access to their critical region of code, and their timeouts aren't intelligent enough to know they are futile.
  *
  *
  *
@@ -12,7 +24,7 @@
 #include <pthread.h>
 #include <stdint.h>
 #include <stdio.h>
-#include <stdlib.h> /*for system()*/
+#include <stdlib.h>
 #include <unistd.h>
 
 /* The LONG_TIMEOUT will be used in the first thread process,
@@ -30,7 +42,7 @@ pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 // uint64_t runsLeft[NUM_THREADS] = { THREAD_ITERATIONS };
 pthread_t threads[NUM_THREADS] = { 0 };
 
-uint64_t resource = 12345678910; // resource being "fought over"
+uint64_t resource = 12345; // resource being "fought over"
 
 /**
  * Basic implementation of Array.indexOf for int arrays
@@ -38,7 +50,7 @@ uint64_t resource = 12345678910; // resource being "fought over"
 int64_t indexOf(pthread_t arr[], pthread_t el)
 {
     for (uint64_t i = 0; i < NUM_THREADS; ++i) {
-        if ((&arr[i]) == &el) {
+        if (arr[i] == el) {
             return i;
         }
     }
@@ -48,6 +60,7 @@ int64_t indexOf(pthread_t arr[], pthread_t el)
 void* thread_func(void* args)
 {
     uint64_t runsLeft = THREAD_ITERATIONS;
+    uint64_t nbrTimesAccessingResource = 0;
     pthread_t pid = pthread_self();
 
     int64_t pidIndex = indexOf(threads, pid);
@@ -55,11 +68,17 @@ void* thread_func(void* args)
     while (runsLeft > 0) {
         if (pthread_mutex_trylock(&mutex) == 0) {
             if (pid == threads[0]) {
-                printf("Longer running thread now in control...");
+                printf("Longer running thread now in control...\n");
             }
             // critical region
-            printf("Thread %i on my %i iteration:\n", pidIndex, runsLeft - THREAD_ITERATIONS);
+            printf("Thread %i on my %i iteration:\n", pidIndex + 1, (THREAD_ITERATIONS - runsLeft) + 1);
             printf("Printing resource: %i\n\n###\n\n", resource);
+
+            // decrement remaining runs
+            --runsLeft;
+            // add to count of times this thread got to access the critical region
+            ++nbrTimesAccessingResource;
+
             if (pid == threads[0]) {
                 // First thread runs longer timeout while keeping mutex locked
                 sleep(LONG_TIMEOUT);
@@ -68,63 +87,33 @@ void* thread_func(void* args)
                 sleep(SHORT_TIMEOUT);
             }
 
-            // decrement remaining runs
-            --runsLeft;
-
             // give up mutex lock
             if (pthread_mutex_unlock(&mutex) != 0) {
-                perror("Couldn't unlock mutex!");
+                perror("Couldn't unlock mutex!\n");
                 pthread_exit(&pid);
             }
         } else {
+            printf("Thread %i putting itself to sleep for %i sec\n", pidIndex + 1, SHORT_TIMEOUT);
+            // Also, decrement here: some threads need to starve so others can eat
+            --runsLeft;
             // set timeout for shorter processes
             sleep(SHORT_TIMEOUT);
         }
     }
+
+    // print all final values
+    printf("\n### Thread %i got to run %i times:\n", pidIndex + 1, THREAD_ITERATIONS - runsLeft);
+    printf("### Thread %i got to access RESOURCE %i times.\n\n", pidIndex + 1, nbrTimesAccessingResource);
+
     return NULL;
 }
-
-// TODO: Need thread 1 func - will take 15 sec
-// void* thread_1(void* args)
-// {
-//     while (1) {
-//         if (pthread_mutex_trylock(mutex) == 0) {
-//             // critical region
-
-//         } else {
-//             // set timeout...
-//             sleep(LONG_TIMEOUT);
-//         }
-//     }
-
-//     sleep(1);
-//     // Check mutex
-// }
-
-// TODO: Need thread 2 func - will take 3 sec
-// void* thread_2(void* args)
-// {
-//     // check mutex
-//     // If locked, set timeout
-// }
-
-// void* manage_threads(void* args)
-// {
-//     // In constant loop, check if any active threads
-// }
-
-// TODO: Need logger func - to keep the thread func's clean
-// Implement print to log.txt file
 
 int main()
 {
     // Create threads
     for (int idx = 0; idx < NUM_THREADS; ++idx) {
-        pthread_create(&threads[idx], NULL, thread_func, NULL);
+        pthread_create(&(threads[idx]), NULL, thread_func, NULL);
     }
-
-    // Manage the threads - prune them and restart them as needed
-    // manage_threads(); // TODO
 
     // Join threads to main
     for (int idx = 0; idx < NUM_THREADS; ++idx) {
