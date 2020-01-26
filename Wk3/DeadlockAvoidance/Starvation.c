8c4918dc2b0d49296b0d17059a1b19233c9185cf
Daniel Cender <danielcender@gmail.com>
2020-01-26 14:29:25 -0600

    feat: finished with program and logging output

--- Wk3/DeadlockAvoidance/Starvation.c
+++ Wk3/DeadlockAvoidance/Starvation.c
@@ -17,14 +17,28 @@
  *
  * So, in this case, our shorter threads never get access to their critical region of code, and their timeouts aren't intelligent enough to know they are futile.
  *
+ * Improvements: If I were to improve upon this so that all the threads could
+ *  gain access to the coveted resource, I would set a backoff for the sleep timeout.
+ *  If the timeout increased by 1 second every time, there'd be an eventual overlap
+ *  with the gap between the longer running thread's runs, so the starved thread could
+ *  hopefully lock the mutex in that time. Better yet, I'd implement a busy wait
+ *  instead of the current check, even though that requires more processing power.
+ *  A spinning lock would knab the resource at some point, but would likely still
+ *  distribute resource access unfairly.
  *
+ * Compiled and ran with:
+ *  ~ gcc -o Starvation Starvation.c -lpthread
+ *  ~ ./Starvation
  *
+ * Logs found in: /log.md
  *
  */
+#include <limits.h>
 #include <pthread.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 
 /* The LONG_TIMEOUT will be used in the first thread process,
@@ -39,10 +53,11 @@
 #define THREAD_ITERATIONS 5 // set iterations to run in each thread created
 
 pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
-// uint64_t runsLeft[NUM_THREADS] = { THREAD_ITERATIONS };
 pthread_t threads[NUM_THREADS] = { 0 };
-
 uint64_t resource = 12345; // resource being "fought over"
+FILE* fptr;
+
+char* cwd[PATH_MAX];
 
 /**
  * Basic implementation of Array.indexOf for int arrays
@@ -62,17 +77,19 @@ void* thread_func(void* args)
     uint64_t runsLeft = THREAD_ITERATIONS;
     uint64_t nbrTimesAccessingResource = 0;
     pthread_t pid = pthread_self();
-
     int64_t pidIndex = indexOf(threads, pid);
 
     while (runsLeft > 0) {
         if (pthread_mutex_trylock(&mutex) == 0) {
             if (pid == threads[0]) {
-                printf("Longer running thread now in control...\n");
+                printf("Longer running thread now in control...\n\n");
+                fprintf(fptr, "_Longer running thread now in control..._\n\n");
             }
             // critical region
-            printf("Thread %i on my %i iteration:\n", pidIndex + 1, (THREAD_ITERATIONS - runsLeft) + 1);
-            printf("Printing resource: %i\n\n###\n\n", resource);
+            printf("Thread %lli on its %llu iteration:\n", pidIndex + 1, (THREAD_ITERATIONS - runsLeft) + 1);
+            fprintf(fptr, "_Thread %lli on its %llu iteration:_\n\n", pidIndex + 1, (THREAD_ITERATIONS - runsLeft) + 1);
+            printf("Printing resource: %llu\n", resource);
+            fprintf(fptr, "- __Printing resource: %llu__\n\n", resource);
 
             // decrement remaining runs
             --runsLeft;
@@ -93,7 +110,8 @@ void* thread_func(void* args)
                 pthread_exit(&pid);
             }
         } else {
-            printf("Thread %i putting itself to sleep for %i sec\n", pidIndex + 1, SHORT_TIMEOUT);
+            printf("Thread %lli putting itself to sleep on its %i run\n", pidIndex + 1, (THREAD_ITERATIONS - runsLeft) + 1);
+            fprintf(fptr, "_Thread %lli putting itself to sleep on its %i run_\n\n", pidIndex + 1, (THREAD_ITERATIONS - runsLeft) + 1);
             // Also, decrement here: some threads need to starve so others can eat
             --runsLeft;
             // set timeout for shorter processes
@@ -101,22 +119,38 @@ void* thread_func(void* args)
         }
     }
 
-    // print all final values
-    printf("\n### Thread %i got to run %i times:\n", pidIndex + 1, THREAD_ITERATIONS - runsLeft);
-    printf("### Thread %i got to access RESOURCE %i times.\n\n", pidIndex + 1, nbrTimesAccessingResource);
+
+    // log all final values
+    printf("\n---\n### Thread %lli final results:\n\n__Thread %lli got to run %llu times.__\n\n__Thread %lli got to access RESOURCE %llu times__\n\n---\n", pidIndex + 1, pidIndex + 1, THREAD_ITERATIONS - runsLeft, pidIndex + 1, nbrTimesAccessingResource);
+    fprintf(fptr, "\n---\n### Thread %lli final results:\n\n__Thread %lli got to run %llu times.__\n\n__Thread %lli got to access RESOURCE %llu times__\n\n---\n", pidIndex + 1, pidIndex + 1, THREAD_ITERATIONS - runsLeft, pidIndex + 1, nbrTimesAccessingResource);
 
     return NULL;
 }
 
 int main()
 {
+    // Get current working directory
+    // Ref: https://stackoverflow.com/questions/298510/how-to-get-the-current-directory-in-a-c-program
+    if (getcwd(cwd, sizeof(cwd)) == NULL) {
+        perror("getcwd() error");
+        return 0;
+    }
+
+    // Open/create new log file - will re-write file on every run
+    if ((fptr = fopen(strcat(cwd, "/log.md"), "w")) == NULL) {
+        perror("File could not be opened or created!");
+        return 0;
+    }
+
+    fprintf(fptr, "## Thread Starvation Program Log File\n\n");
+
     // Create threads
-    for (int idx = 0; idx < NUM_THREADS; ++idx) {
+    for (uint64_t idx = 0; idx < NUM_THREADS; ++idx) {
         pthread_create(&(threads[idx]), NULL, thread_func, NULL);
     }
 
     // Join threads to main
-    for (int idx = 0; idx < NUM_THREADS; ++idx) {
+    for (uint64_t idx = 0; idx < NUM_THREADS; ++idx) {
         pthread_join(threads[idx], NULL);
     }
 
