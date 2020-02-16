29ffc57f090b6b75e4ff618fb2be8206b3b14c54
Daniel Cender <danielcender@gmail.com>
2020-02-15 21:11:31 -0600

    Week 6 (#9)
    * chore: script to print all hard linked files of files in disk
    
    * feat: finished with wk6

---
+++ Wk6/DQ2.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#
+# Description: This program goes to the root directory and recursively gets all files
+# 	with 2 or more hard links, displaying their linked nodes to the console.
+# Author: Daniel Cender
+# Class: CST-221-O500 - Operating Systems Concepts
+#
+
+# Assigns output of pwd to WORKING_DIR, so we can get back to where we started
+WORKING_DIR=`pwd`
+
+echo "Current Directory (will return here after process...): "
+echo $WORKING_DIR
+# Go to the root directory
+~
+
+for i in $(find / -type f -links +2 -printf '%i'):
+do
+	echo "File contains >=2 hard links: $i"
+	INODE=$(ls -i $i | awk '{print $1}')
+	CONNECTED_FILES=$(find / -inum $INODE)
+	echo $CONNECTED_FILES
+done
+
+
+cd $WORKING_DIR
