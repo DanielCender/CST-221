13bfd9dfce8bfa35d4c4f1f497a684b855246eff
Daniel Cender <danielcender@gmail.com>
2020-02-22 17:46:41 -0500

    fix: this fixes everything

--- Wk7/management.sh
+++ Wk7/management.sh
@@ -20,36 +20,51 @@ userFile=$1
 groupName=$2
 opFlag=$3
 
+echo "Passed in $opFlag"
+
+if [[ $opFlag != "-a" && $opFlag != "-r" ]]; then
+	# This is an invalid 3rd argument, exit script
+	echo "Third argument must be either an -a (add users) or -r (remove users) flag"
+	exit
+fi
+
 # Measures nbr of arguments in commands passed into script
-# If commnads are
-if [ $# -ne 3 ]; then
+if [[ $# -ne 3 ]]; then
     echo "Your command line contains $# arguments."
 		echo "Please pass in the 3 required args: (user filename, group name, operation flag)!"
 else
 	# Operate on users as specified
-
+	echo "Checking for valid group"
 	# Check if user group already exists
-	if grep $group /etc/group
-    then
-         echo "User group exists"
-    else
-         echo "Creating user group"
-				 groupadd $groupName
-    fi
-
-	# Parse out input file
+	if echo "$OUTPUT" | grep -q $groupName /etc/group; then
+         echo "User group already exists"
+		if [[ $opFlag == "-r" ]]; then
+			echo "Deleting user group now"
+			# Force the group's deletion
+			groupdel $groupName
+		fi
+    	else
+         	if [[ $opFlag == "-a" ]]; then
+		echo "Creating user group"
+	 	groupadd $groupName
+		fi
+    	fi
+		# Parse out input file
 	cat "${userFile}" | while IFS=" " read username password
 	do
-    echo "user='$username' will now be created if they don't exist already..."
-    # do something
+    		[[ $opFlag == "-a" ]] && echo "user '$username' will now be created if they don't exist already..."
+		[[ $opFlag == "-r" ]] && echo "user '$username' will now be deleted if they exist already"
 		cat /etc/passwd | grep ${username} >/dev/null 2>&1
-		if [ $? -eq 0 ] ; then
-    	echo "$username Already Exists!"
-			usermod -a -G $groupName $username
+		if [[ $? -eq 0 ]] ; then
+    			echo "$username Already Exists!"
+			[[ $opFlag == "-a" ]] && usermod -a -G $groupName $username
+			# The -r option on userdel removes mail spools
+			#  and user directories for us
+			[[ $opFlag == "-r" ]] && userdel -r $username
 		else
-    	echo "User Not Found"
+    			echo "User Not Found"
 			# create user with home directory and group assignment
-			useradd -m -G $groupName -p $password $username
+			[[ $opFlag == "-a" ]] && useradd -m -G $groupName -p $password $username
 		fi
 	done
 	echo "*** User management completed! ***"
